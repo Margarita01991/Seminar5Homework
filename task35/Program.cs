414ce5e34b8da22c1bb589538d64c0f5b33a6a65
Margarita <margarita27011991@gmail.com>
2022-08-22 16:20:02 +0300

    количество элементов массива, значения которых лежат в отрезке [10,99]

--- task35/Program.cs
+++ task35/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123*/
+int[] FillArray(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int [] arr = new int [size];
+    for(int i = 0; i <arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange +1);
+    }
+    return arr;
+}
+int[] array = FillArray (12, 0, 300);
+Console.WriteLine('['+ string.Join(", ", array) + ']');
+int count = 0;
+for(int i = 0; i <array.Length; i++)
+{
+    if(array[i] >= 10 && array[i] < 100)
+    count++;
+}
+Console.WriteLine($"В отрезке [10,99] находятся {count} элементов");

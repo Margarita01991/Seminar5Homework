672e9ca39473964811a1ecdf5fc5bdc2ed04feff
Margarita <margarita27011991@gmail.com>
2022-08-22 15:54:17 +0300

    показывает количество чётных чисел в массиве случайных трехзначных чисел

---
+++ task34/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+Console.Clear();
+int[] FillArray(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int [] arr = new int [size];
+    for(int i = 0; i <arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange +1);
+    }
+    return arr;
+}
+
+int[] array = FillArray (8,100, 1000);
+Console.WriteLine('['+ string.Join(", ", array) + ']');
+int count = 0;
+for(int i = 0; i <array.Length; i++)
+{
+    if (array[i]%2 ==0)
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);

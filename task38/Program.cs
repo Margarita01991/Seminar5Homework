560322c6d5956691b2f98ca10a4b2f516ccea75c
Margarita <margarita27011991@gmail.com>
2022-08-22 18:02:03 +0300

    айдите разницу между максимальным и минимальным элементов массива

---
+++ task38/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Задача 38: Задайте с клавиатуры массив вещественных (int) чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76 */
+Console.Clear();
+double[] array = new double[] {5.3, -30.12, 99.2, 205.2, 32, 4.1};
+Console.WriteLine('['+ string.Join("  ", array) + ']');
+
+double min = 0;
+double max = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+        {
+            max = array[i];
+        }
+    if (array[i] < min)
+        {
+            min = array[i];
+        }
+}
+Console.WriteLine($"Разница между максимальным и минимальным элементом' = {max - min}");
+
+
+
+// for (int i = 0; i <array.Length - 1; i++) 
+// {
+//     double minposition = i;
+//     {
+//         for (int j = i + 1 ; j <array.Length; j++)
+//         {
+//         if (array[j] < array[minposition]) minposition = j;
+//         }
+//     }
+
+// }
